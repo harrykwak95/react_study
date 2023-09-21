9b0014e4224511dd0368e419f828745f226195a1
곽민우 <harrykwak@kakao.com>
2023-09-22 01:36:55 +0900

    feat : CRA, 컴포넌트 분리, CSS 모듈

---
+++ cra/super_cool/src/App.js
@@ -0,0 +1,14 @@
+import Button from "./Button";
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={styles.title}>hi</h1>
+      <Button text={'DRAGON'}/>
+
+    </div>
+  );
+}
+
+export default App;
